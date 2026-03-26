579dc817e317b0fff77a008512cc328ccfe53121
hamda <hamda@techmac.co.uk>
2026-03-26 20:02:09 +0500

    Fix: Improve error handling in React initialization

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { HashRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
-)
+const root = document.getElementById('root')
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+  )
+} else {
+  console.error('Root element not found!')
+}
